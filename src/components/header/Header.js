4b0fd33040fca8a4a3014de687705c4e1d460436
petreasil <42999818+petreasil@users.noreply.github.com>
2020-03-23 19:28:50 +0200

    headerJS modify

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -33,12 +33,14 @@ function Header(props) {
 
       <div className="my-2">
         <h6 className="text-center text-white">Global Cases: {startTime()}</h6>
-        <span className="badge badge-pill badge-primary">{data.cases}</span>
+        <span className="badge badge-pill badge-primary">
+          Cases: {data.cases}
+        </span>
         <span className="badge badge-pill badge-danger ml-2">
-          {data.deaths}
+          Deaths: {data.deaths}
         </span>
         <span className="badge badge-pill badge-primary ml-2">
-          {data.recovered}
+          Recovered: {data.recovered}
         </span>
       </div>
 
